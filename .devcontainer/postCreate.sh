a5ad2be2c362cd03ab8752fa00089d4120bf32ff
luis12duboqwe <12luisdubon@gmail.com>
2025-10-22 20:12:18 -0600

    chore(devcontainer): instala dependencias backend en postCreate [v2.2.0]

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -11,8 +11,9 @@ fi
 
 source .venv/bin/activate
 python -m pip install --upgrade pip
+python -m pip install -r backend/requirements.txt
 python -m pip install -r requirements.txt
-python -m pip install pydantic-settings==2.2.1 pytest==8.1.1
+python -m pip install pytest==8.1.1
 
 deactivate
 
