ca18134e8a9ff0f1dd17ec97d61ead03aaa03cf7
luis12duboqwe <12luisdubon@gmail.com>
2025-10-27 13:51:46 -0600

    feat(frontend): configura eslint [v2.2.0]

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/prop-types': 'off',
+    'no-console': 'warn'
+  }
+};
