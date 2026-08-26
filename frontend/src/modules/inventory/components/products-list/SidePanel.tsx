09c5ed2a82c765a00f6f819beae30c442f5e99c6
SoftMobile <12luisdubon@gmail.com>
2026-08-26 14:54:57 -0600

    REC-0005: use stable Button alias in product side panel

--- frontend/src/modules/inventory/components/products-list/SidePanel.tsx
+++ frontend/src/modules/inventory/components/products-list/SidePanel.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Button from "../../../../../components/ui/Button";
+import Button from "@components/ui/Button";
 import StatusBadge from "./StatusBadge";
 import StockBadge from "./StockBadge";
-import "../../InventoryTable.css"; // Ensure styles are loaded
+import "../../InventoryTable.css";
 
 type ProductInfo = {
   name?: string;
